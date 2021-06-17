f93f366b52b75555a251ecaf9955e302dba5a943
Your Name <you@example.com>
2021-06-17 14:28:29 +0300

    increase version by tovi@openapp

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021021701;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2021021708;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2016052300;      // Requires this Moodle version.
 $plugin->component = 'local_video_directory';// Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
